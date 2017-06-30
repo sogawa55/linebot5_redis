2b0b81fdbce15efdf74840f63a0212574ca23edf
sogawa <s.ogawa055@gmail.com>
2017-06-30 21:31:29 +0000

    modify

--- lib/docomo_client.rb
+++ lib/docomo_client.rb
@@ -5,9 +5,9 @@ class DocomoClient
     @api_key = api_key
   end
  
-  def chat(message, context=nil,mode="dialog")
+  def chat(message, mode="dialog",da=nil,context=nil,)
     client = Docomoru::Client.new(api_key: ENV["DOCOMO_API_KEY"])
-    response = client.create_dialogue(message,context:context,mode:mode)
+    response = client.create_dialogue(message, {mod=>mode, da=>da, context=>context} )
  
     return response
   end
